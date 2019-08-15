7c2000bf13298607e88a8555ba4140ec07daadf3
Phyllipe Bezerra <pmba@ic.ufal.br>
2019-08-14 23:27:42 -0300

    require erro

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const express       = require('express');
 const bodyParser    = require('body-parser');
 const app           = express();
 
-const Config  = require('./config') || {
-    consumer_key        : process.env.CONSUMER_KEY,
-    consumer_secret     : process.env.CONSUMER_SECRET,
-    access_token_key    : process.env.ACCESS_TOKEN_KEY,
-    access_token_secret : process.env.ACCESS_TOKEN_SECRET
-};
+var Config = {};
+
+try {
+    Config = require('./config');
+} catch (error) {
+    Config = {
+        consumer_key        : process.env.CONSUMER_KEY,
+        consumer_secret     : process.env.CONSUMER_SECRET,
+        access_token_key    : process.env.ACCESS_TOKEN_KEY,
+        access_token_secret : process.env.ACCESS_TOKEN_SECRET
+    };
+}
 
 const T       = new Twitter(Config);
 const Port    = process.env.PORT || 5000;
